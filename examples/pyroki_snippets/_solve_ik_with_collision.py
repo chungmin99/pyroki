74db40b7ef5cc67bcac7e6a7a394afb1c576e032
Chung Min Kim <chungmin99@gmail.com>
2026-01-05 15:31:28 -0800

    Remove explicit termination config from IK collision solver

--- examples/pyroki_snippets/_solve_ik_with_collision.py
+++ examples/pyroki_snippets/_solve_ik_with_collision.py
@@ -104,10 +104,6 @@ def _solve_ik_with_collision_jax(
     sol = (
         jaxls.LeastSquaresProblem(costs=costs, variables=variables)
         .analyze()
-        .solve(
-            verbose=False,
-            linear_solver="dense_cholesky",
-            termination=jaxls.TerminationConfig(max_iterations=50),
-        )
+        .solve(verbose=False, linear_solver="dense_cholesky")
     )
     return sol[joint_var]
